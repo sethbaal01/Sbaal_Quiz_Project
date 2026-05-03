1434e0c1668f361cedff1df92290a72268a297c6
SethBaal00 <509sbaal@student.angelina.edu>
2026-05-03 18:53:08 -0500

    final project commit

--- src/Quiz.py
+++ src/Quiz.py
@@ -4,6 +4,8 @@
 #When building new quizzes, refer to the formatting guidlines at the top of the defualt 'quiz.txt' file
 
 import os
+from colorama import Fore, Style, init
+init(autoreset=True)
 
 #This function handes actually reading the files and 
 def loadQuestions(filePath):
@@ -34,19 +36,20 @@ def runQuiz(questions):
     score = 0
     
     for i, (question, answer) in enumerate(questions, start=1):
-        print(f"\nQuestion {i}: {question}")
+        print(Style.BRIGHT + Fore.BLUE + f"\nQuestion {i}: {question}")
         userAnswer = input("Your answer: ").strip()
         
         if userAnswer.lower() == answer.lower(): #.lower() handles caps checking
-            print("Correct!")
+            print(Fore.GREEN + "Correct!")
             score += 1
         else:
-            print(f"Incorrect!\nCorrect answer: {answer}")
+            print(Style.BRIGHT + Fore.RED + f"Incorrect!\nCorrect answer: {answer}")
     
     return score
 
 #This is the main function, just calling this runs the whole thing
 def main():
+
     defaultPath = "quiz.txt"
     userPath = input(f"Enter a File path for quiz (Press enter for default quiz: {defaultPath}): ")
     
@@ -72,9 +75,9 @@ def main():
     total = len(questions)
     percentage = (score / total) * 100
     
-    print("\nResults:")
-    print(f"Score: {score}/{total}")
-    print(f"Percentage: {percentage:.2f}%")
+    print(Fore.CYAN + "\nResults:")
+    print(Fore.CYAN + f"Score: {score}/{total}")
+    print(Fore.CYAN + f"Percentage: {percentage:.2f}%")
     
 
 if __name__ == "__main__":
